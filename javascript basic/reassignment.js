b1a851dcbafb3705cb0896f7a81c222ce32cb663
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-07-04 21:18:40 +0000

    reassignment and redeclaration

---
+++ javascript basic/reassignment.js
@@ -0,0 +1,13 @@
+// reassignment and redeclaration 
+
+var a = 12;
+a = 32;
+
+var a = 233; // redeclaration is possible
+
+
+let b = 12;
+b = 32;
+
+let a = 2334; // redeclaration is not possible 
+

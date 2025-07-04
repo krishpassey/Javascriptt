7fa7de42377262f99767e81b05645c9f8ea4c458
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-07-04 21:01:13 +0000

    variables

---
+++ javascript basic/variables.js
@@ -0,0 +1,27 @@
+// var, let, const - line-by-line comparison 
+
+a = 12; // not a right way 
+var a;
+var a = 12; 
+let a = 12;
+let a;
+const a = 12;
+
+
+// declarations and initialisation
+
+var a; // declare karna 
+var a = 12; // declare and initialise 
+// same happens with let as well 
+
+
+var a = 12; 
+// window mein add hota hai 
+// function scoped hota hai 
+// we can declare it once again and eroor will not come 
+
+
+const discount = 12;
+// it can not be changed, the value is constant 
+
+

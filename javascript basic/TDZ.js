7d8b122fae7b6c2e1a0eb55733d5cfbc086d9aaa
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-07-04 21:29:43 +0000

    Temporal dead zone

---
+++ javascript basic/TDZ.js
@@ -0,0 +1,13 @@
+// Temporal dead zone.... the area in which js knows that variable exists but 
+// it will not be able to give you its value 
+
+console.log(a);
+// ERROR: a is not defined
+
+
+let a = 12;
+// ERROR: can not access a before initialisation 
+
+
+var a = 13; // undefined 
+// only be done in let and const but not be done in var 

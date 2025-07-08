5eb1643d35e4d427d2ebb8147dd32628aec4753a
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-07-08 20:38:21 +0000

    dynamic typing

---
+++ Data types/dynamic_typing.js
@@ -0,0 +1,16 @@
+
+// dynamic typing 
+// js do not have ststic typing like other languages 
+// we can change data because there are dynamic data types 
+
+
+// typeof quirks 
+
+
+// typeof NaN = number 
+// NaN === NaN -> false 
+// [] + [] = ''
+// '1' + "1" = "11"
+// 1 == "1" = true
+// 1 === "1" = false 
+// typeof null = object 

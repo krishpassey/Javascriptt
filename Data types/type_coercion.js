8043e391796c37152ab90841717be5eb4874155d
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-07-08 20:53:39 +0000

    type coersion

---
+++ Data types/type_coercion.js
@@ -0,0 +1,18 @@
+
+type coersion -> this is a concept in which one type is automatically converted ...
+
+// "5" = 1 -> "51"
+// this is because + can do both work 'adding' and 'concatination'
+
+
+// "5" - 1 -> 4
+// this is because - can only be used to subtract 
+
+
+Truthy vs falsy values ...
+// falsy values -> 0, false, "", null, undefined, document.all, NaN
+
+// rest of the values are converted in truthy
+
+
+to check -> !!0  (false)

4ed31595d0f66426a7f317a2aa881d3c401c521b
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-07-09 20:36:51 +0000

    types of operator

---
+++ Operators /types.js
@@ -0,0 +1,54 @@
+
+// Arithmatic operators -> ( + - / * % ** )
+
+// comparison -> (= == == != !== >= <= < > )
+// = -> inserting value 
+
+12 == "12" // true
+// == only checks the values not type
+// not strict comparison
+
+12 === "12" // false
+// === checks the type and value
+// strict comparison 
+
+12 != "12" //false
+
+12 !== 12 // true 
+
+
+22 >= 22 // true 
+
+32 <= 32 // true 
+
+
+// assignment operator -> (= += -= *= /= %=)
+
+let a =12;
+a += 3; //15
+a -= 11; //4
+a *= 2; //8
+a /= 2; //4
+a %= 3; //1
+
+
+// logical operators -> ( && || !)
+
+// AND operator 
+true && true => true
+true && false => false 
+false&& false => false 
+
+
+// OR operator 
+false || true => true 
+
+
+
+// unary operator ( + - ! typeof ++ -- )
+
+// ternary operator (?:)
+condition ? true hone ka code : false hone ka code 
+12 > 13 ? console.log("true") : console.log("false") // -> false 
+
+

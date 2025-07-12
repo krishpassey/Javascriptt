249716beb510b253ebb632e8b2052d617d99d353
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-07-12 19:19:47 +0000

    basics

---
+++ Loops /loops.js
@@ -0,0 +1,21 @@
+// kch bhi repeat krne ko loop kehte hai 
+// for while do-while foreach forof forwe 
+
+// 1 1 1 1 1 1 1
+// 1 2 3 4 5 6 7 
+
+
+// For loop
+// kaha se jana hai -> kaha tak jana hai -> kaise jana hai 
+
+
+// While loop
+// kaha se jana hai -> kab sukna hai -> kaise jana hai 
+
+
+// 1- 40 for
+// 20 - 30 for
+// hello na aajaye - while
+// laal clr ka ped - while
+
+

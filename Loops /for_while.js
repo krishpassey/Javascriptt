2fcbb824543d207d405735c1931d5cbbfc774e8f
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-07-12 19:35:10 +0000

    for,while and do while loop

---
+++ Loops /for_while.js
@@ -0,0 +1,32 @@
+// For loop 
+// 1 - 100
+
+for ( let i = 1; i<101; i++) {
+    console.log(i);
+}
+
+// While loop 
+start
+while(end) {
+    //code
+    change
+}
+
+// create loop which starts from 1 and stops when 32 comes
+let i = 1;
+while(i<32) {
+    // CODE
+    console.log(i);
+    i++;
+}
+
+
+// Do While
+let i = 12;
+do {
+    console.log(i);
+    i++;
+}
+while(i<2
+    // it will run atleast once then also if the condition is false 
+)

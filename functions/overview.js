c56d57fd71078e312de0fc6d3f9104765057ad6d
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-07-14 19:52:22 +0000

    overview

---
+++ functions/overview.js
@@ -0,0 +1,22 @@
+
+// How to make function
+
+function hny() {  // function statements
+         console.log("happy new year");
+}
+hny();
+
+
+let fnc = function(){ //function expression
+    console.log("hehehe")
+}
+fnc();
+
+
+// fat arrow function
+let fnc = () => {
+    console.log("heheheh");
+}
+fnc();
+
+

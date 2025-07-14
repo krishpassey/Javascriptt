46ff2f1c5704879a2802ce90c9f2462249e7138e
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-07-14 19:57:52 +0000

    Parameter and Arguments

---
+++ functions/function1.js
@@ -0,0 +1,19 @@
+
+function dance (v1) {
+    console.log(`${v1} is dancing`);
+}
+
+dance("I");
+dance("Dev");
+dance("ishant");
+dance("dhruv");
+
+
+
+function add(v1,v2){  // Parameters
+    console.log(v1+v2);
+}
+// Arguments
+add(1,2);  
+add(11,22);
+add(111,22);

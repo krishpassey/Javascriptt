e63cc189769a7f2b22a3ba5d34856fdd2b0cebee
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-08-27 18:55:00 +0000

    array basics

---
+++ arrays/arrays1.js
@@ -0,0 +1,33 @@
+
+// modifying array
+let arr = [1, 2, 3, 4];
+arr [2] = 12;
+
+
+// array methods 
+// push
+let arr = [1, 2, 3, 4, 5];
+arr.push(20);
+
+
+// pop
+let arr = [1, 2, 3, 4, 5];
+arr.pop();
+
+// shift -> removes starting val of array
+let arr = [1,2,3,4,5];
+arr.shift();
+
+//unshift -> adds a value in starting
+let arr = [1, 2, 3, 4, 5];
+arr.unshift();
+
+// spice -> removes value from between array
+let arr = [1, 2, 3, 4, 5];
+arr.spice(2,1);
+
+// slice -> it creates a new array after removing the desired values 
+let arr = [1, 2, 3, 4, 5];
+let newarr = arr.slice(0,3);
+
+

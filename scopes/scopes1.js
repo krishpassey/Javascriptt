6f414f51b1283c958605354eb4bf62ffa3a50578
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-09-04 17:08:02 +0000

    scopes

--- scopes/scopes1.js
+++ scopes/scopes1.js
@@ -9,3 +9,48 @@ if(true) {
 console.log(a);
 //console.log(b);
 //console.log(c);
+
+
+
+function one(){
+    const username = "krish"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+one()
+
+
+
+
+if (true) {
+    const username = "krish"
+    if(username === "krish") {
+        const website = "youtube"
+        console.log(username + website);
+    }
+    //console.log(website);
+}
+// console.log(username);
+
+
+
+// ++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++=
+
+addone(5)
+function addone(value) {
+      return num + 1 
+}
+//  addone(5); // can be accessed this way 
+
+
+addTwo(5) // not accessed this way
+ const addTwo = function(num){
+    return num + 2 
+ }
+  

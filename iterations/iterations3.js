4cdc32c1ecd254997b0e9e00792466ce016b5070
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-09-05 21:58:43 +0000

    maps

---
+++ iterations/iterations3.js
@@ -0,0 +1,43 @@
+
+const arr = [1, 2, 3, 4, 5]
+
+for(const num of arr) {
+    console.log(num);
+}
+
+
+
+
+const greetings = "hello world"
+for(const greet of greetings) {
+    console.log(`each char is ${greet}`)
+}
+
+
+
+// maps
+
+const map = new map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+map.set('IN', "India")
+
+console.log(map);
+
+for(const [key, value] of map) {
+    console.log(key, `:-`, value);
+}
+
+
+
+
+const myObject = {
+    'game1': 'NFS',
+    'game2': spiderman
+}
+// for(const [key, value] of map) {
+//    console.log(key, `:-`, value);
+
+
+

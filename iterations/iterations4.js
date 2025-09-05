812773d7619db73058b771be52ec0db17186d2ca
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-09-05 22:08:01 +0000

    for in loop

---
+++ iterations/iterations4.js
@@ -0,0 +1,29 @@
+// for in
+const myObject = {
+    js: 'javascript',
+    cpp:'c++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    console.log(key);
+}
+
+
+
+
+const programming = ["js", "rb", "py", "java"]
+
+for (const key in programming) {
+    console.log(key);
+}
+
+
+
+
+
+
+
+
+

12bc1b0424a57ee41bf24c2ce3ee894f50d41a84
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-09-24 21:31:32 +0000

    practice 3

--- events/script2.js
+++ events/script2.js
@@ -5,9 +5,11 @@ let currMode = "light"; // dark
 modeBtn.addEventListener("click", () => {
     if(currMode === "light") {
         currMode = "dark";
+        document.querySelector("body").style.backgroundColor = "black";
     }
     else {
         currMode = "light";
+        document.querySelector("body").style.backgroundColor = "white";
     }
     console.log(currMode);
 });

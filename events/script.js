2f292ea3e4aa7da1c3acff663f33d3a0e4ad11d0
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-09-24 21:21:56 +0000

    practice 2

--- events/script.js
+++ events/script.js
@@ -1,7 +1,17 @@
-let btn1 = document.querySelector("#btn1");
+/* let btn1 = document.querySelector("#btn1");
 btn1.onclick = () => {
     console.log("btn1 was clicked");
     let a = 25;
     a++;
     console.log(a); //26
-}
+} */
+
+   const button1 = (evt) => {
+        console.log("btn1 was clicked")
+        console.log("evt.type");
+    };
+
+    btn1.addEventListener ("click", button1);
+
+    btn1.removeEventListener("click", button1);
+

fb3ac2a617a638e3024df488dbe0aa1286399f97
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-09-25 23:00:49 +0000

    promises

---
+++ promises/promises1.js
@@ -0,0 +1,13 @@
+const promiseOne = new Promise(function(resolve, reject){
+    // Do an async task
+    // Db calls, cryptography, network
+
+    setTimeout(function(){
+        console.log('Async task is completed');
+        resolve()
+    }, 1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})

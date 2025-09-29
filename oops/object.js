a3566a95353f073a3a18744c25206017b9cfa70e
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-09-29 21:16:59 +0000

    prototype

--- oops/object.js
+++ oops/object.js
@@ -9,17 +9,38 @@ console.log(multipleBy5.power);
 console.log(multipleBy5.prototype)
 
 
+
 function createUser(username, score) {
     this.username = username
     this.score = score
 }
 
 createUser.prototype.increment = function() {
-    this.scorea++
+    this.score++
 }
 createUser.prototype.printMe = function(){
-    console.log(`score is ${this.score}`);
+    console.log(`price is ${this.score}`);
 }
 
-const chai = createUser("chai", 25)
-const tea = createUser("tea", 250)
+const chai = new createUser("chai", 25)
+const tea = new createUser("tea", 250)
+
+chai.printMe();
+
+
+/*
+This is what happend behind the scenes when the new keywordn is used:
+
+A new object is created: the new keyword initiates the creation of newJS object
+
+A prototype is linked: the newly created object gets linked to the prototype property
+of the constructor function. This means that it has access to properties and methods 
+defined on the constructor's prototype
+
+The constructor is called: the constructor function is called with specified arguments 
+and this is bound to the newly created object. If no explicit return value is specified
+from the constructor, JS assumes this, the newly created object, to be the intended return value.
+
+The new object is returned: After the constructor function has been called, if it doesn't return a 
+non - primitive value (object, array, function, etc.), the newly created object is returned
+*/

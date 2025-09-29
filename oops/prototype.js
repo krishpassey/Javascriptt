f605cf2f34da44c44e6e98058e6d96e888c5bd68
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-09-29 21:45:38 +0000

    prototype

--- oops/prototype.js
+++ oops/prototype.js
@@ -23,3 +23,32 @@ Array.prototype.heyKrish = function () {
 myHeros.krish()
 myHeros.heyKrish()
 //HeroPower.heyKrish()
+
+
+
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvaliable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher);
